3ef076c8b508d1eb8d29440b629b7badd02897d5
Christoffer Rydberg <christoffer.e.rydberg@gmail.com>
2021-06-23 19:55:50 +0200

    Fix error when resolving paths in resource manager

--- demos/network-game-demo-desktop/src/main/java/com/gamelibrary2d/demos/networkgame/DesktopResourceManager.java
+++ demos/network-game-demo-desktop/src/main/java/com/gamelibrary2d/demos/networkgame/DesktopResourceManager.java
@@ -16,6 +16,25 @@ import java.util.stream.Stream;
 
 public class DesktopResourceManager implements ResourceManager {
 
+    private static List<String> getResources(Path fileSystemPath, Path resourceFolder) throws IOException {
+        int resourcePathLength = 1 + resourceFolder.getNameCount();
+        List<String> resources = new ArrayList<>();
+        try (Stream<Path> walk = Files.walk(fileSystemPath, 1)) {
+            for (Iterator<Path> it = walk.iterator(); it.hasNext(); ) {
+                Path fullPath = it.next().toAbsolutePath();
+                String normalizedFullPath = fullPath.toString().replace('\\', '/');
+                String normalizedResourceFolder = resourceFolder.toString().replace('\\', '/');
+                if (!normalizedFullPath.endsWith(normalizedResourceFolder)) {
+                    int fullPathLength = fullPath.getNameCount();
+                    Path resourcePath = fullPath.subpath(fullPathLength - resourcePathLength, fullPathLength);
+                    resources.add(resourcePath.toString());
+                }
+            }
+        }
+
+        return resources;
+    }
+
     @Override
     public InputStream open(String resource) throws IOException {
         return this.getClass().getResource(String.format("/%s", resource)).openStream();
@@ -30,29 +49,12 @@ public class DesktopResourceManager implements ResourceManager {
             throw new IOException(String.format("Failed to create URI for resource path: '%s'", path), e);
         }
 
-        Path fileSystemPath;
         if (uri.getScheme().equals("jar")) {
             try (FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap())) {
-                fileSystemPath = fileSystem.getPath(String.format("/%s", path));
+                return getResources(fileSystem.getPath(String.format("/%s", path)), Paths.get(path));
             }
         } else {
-            fileSystemPath = Paths.get(uri);
+            return getResources(Paths.get(uri), Paths.get(path));
         }
-
-        Path resourceFolder = Paths.get(path);
-        int resourcePathLength = 1 + resourceFolder.getNameCount();
-        List<String> resources = new ArrayList<>();
-        try (Stream<Path> walk = Files.walk(fileSystemPath, 1)) {
-            for (Iterator<Path> it = walk.iterator(); it.hasNext(); ) {
-                Path fullPath = it.next();
-                int fullPathLength = fullPath.getNameCount();
-                Path resourcePath = fullPath.subpath(fullPathLength - resourcePathLength, fullPathLength);
-                if (!resourcePath.endsWith(resourceFolder)) {
-                    resources.add(resourcePath.toString());
-                }
-            }
-        }
-
-        return resources;
     }
 }
