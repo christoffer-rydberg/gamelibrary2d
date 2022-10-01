46403c0943e58aab34b27d4798d7f4a55c87daf0
Christoffer Rydberg <christoffer.e.rydberg@gmail.com>
2022-10-01 12:16:01 +0200

    Improve navigation panel

--- api/gamelibrary2d-core/src/main/java/com/gamelibrary2d/components/containers/NavigationPanel.java
+++ api/gamelibrary2d-core/src/main/java/com/gamelibrary2d/components/containers/NavigationPanel.java
@@ -3,7 +3,6 @@ package com.gamelibrary2d.components.containers;
 import com.gamelibrary2d.common.Point;
 import com.gamelibrary2d.common.Rectangle;
 import com.gamelibrary2d.components.AbstractGameObject;
-import com.gamelibrary2d.components.GameObject;
 import com.gamelibrary2d.components.denotations.*;
 import com.gamelibrary2d.framework.Renderable;
 
@@ -13,12 +12,12 @@ import java.util.Deque;
 public class NavigationPanel extends AbstractGameObject
         implements Clearable, PointerDownAware, PointerMoveAware, PointerUpAware {
     private final Point transformationPoint = new Point();
-    private final Deque<GameObject> previous = new ArrayDeque<>();
-    private GameObject current;
+    private final Deque<Renderable> previous = new ArrayDeque<>();
+    private Renderable current;
     private Rectangle bounds;
 
-    public void navigateTo(GameObject obj, boolean rememberPrevious) {
-        GameObject previous = this.current;
+    public void navigateTo(Renderable obj, boolean rememberPrevious) {
+        Renderable previous = this.current;
         if (previous != obj) {
             this.current = obj;
             if (rememberPrevious && previous != null) {
@@ -34,7 +33,7 @@ public class NavigationPanel extends AbstractGameObject
     }
 
     public void goBack(int steps) {
-        GameObject previous = null;
+        Renderable previous = null;
 
         while (steps > 0 && !this.previous.isEmpty()) {
             --steps;
@@ -87,32 +86,38 @@ public class NavigationPanel extends AbstractGameObject
         }
     }
 
-    private void navigatedFrom(GameObject obj) {
+    private void navigatedFrom(Renderable obj) {
         NavigationAware navigationAware = asNavigationAware(obj);
         if (navigationAware != null) {
             navigationAware.navigatedFrom(this);
         }
     }
 
-    private void navigatedTo(GameObject obj) {
+    private void navigatedTo(Renderable obj) {
         NavigationAware navigationAware = asNavigationAware(obj);
         if (navigationAware != null) {
             navigationAware.navigatedTo(this);
         }
     }
 
-    private NavigationAware asNavigationAware(GameObject obj) {
+    private NavigationAware asNavigationAware(Renderable obj) {
         return obj instanceof NavigationAware ? (NavigationAware) obj : null;
     }
 
     @Override
     public Renderable getRenderer() {
-        return current.getRenderer();
+        return current;
     }
 
     @Override
     public Rectangle getBounds() {
-        return bounds != null ? bounds : current.getBounds();
+        return bounds != null ? bounds : getCurrentBounds();
+    }
+
+    private Rectangle getCurrentBounds() {
+        return current instanceof Bounded
+            ? ((Bounded) current).getBounds()
+            : Rectangle.EMPTY;
     }
 
     public void setBounds(Rectangle bounds) {
